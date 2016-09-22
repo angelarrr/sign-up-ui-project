3faf638089a8d5af94e3edf13f5ab16a37e06647
angelarrr <kwon.angela@gmail.com>
2016-09-22 15:02:10 -0400

    select all box toggle

--- js/app.js
+++ js/app.js
@@ -7,12 +7,24 @@ $(function(){
     });
   }
 
+  // select all button toggle
+  $.fn.selectBoxToggle = function(selectbox){
+    var $checkboxes = this;
+
+    return $checkboxes.change(function(){
+      var check = $checkboxes.filter(':checked').length == $checkboxes.length;
+      $(selectbox).prop('checked', check);
+    });
+  }
+
   var $subjectInput = $('input[name=subject]');
   var $gradeInput = $('input[name=grade]');
 
   // toggle subjects
   $('#check-all').checkboxToggle($subjectInput);
+  $subjectInput.selectBoxToggle('#check-all');
 
   // toggle grades
   $('#all').checkboxToggle($gradeInput);
+  $gradeInput.selectBoxToggle('#all');
 });
